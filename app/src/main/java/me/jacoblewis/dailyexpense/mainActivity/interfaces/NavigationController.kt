61dadf11019b18208e44f19ffa25530c51999d17
Jacob <zerogamerfx@gmail.com>
2018-11-24 13:05:59 -0600

    simplified navigation

--- app/src/main/java/me/jacoblewis/dailyexpense/mainActivity/interfaces/NavigationController.kt
+++ app/src/main/java/me/jacoblewis/dailyexpense/mainActivity/interfaces/NavigationController.kt
@@ -13,17 +13,11 @@ import me.jacoblewis.dailyexpense.mainActivity.interfaces.nav.NavScreen
 import me.jacoblewis.dailyexpense.mainActivity.interfaces.nav.RootFragment
 import java.util.*
 
-interface NavigationController : ActivityController {
-    val fragmentFrame: Int
-    fun linkToolBarToDrawer(toolbar: Toolbar)
+interface NavigationController : NavigationHandler {
 
-    companion object {
-        val rootFragmentStack: Stack<RootFragment> by lazy { Stack<RootFragment>() }
-    }
-
-    fun navigateTo(navScreen: NavScreen, navBack: Boolean = false) {
+    fun navigateTo(navScreen: NavScreen) {
         when (navScreen) {
-            is NavScreen.Main -> openMain(navBack)
+            is NavScreen.Main -> openMain()
             is NavScreen.Settings -> openSettings()
             is NavScreen.EnterCategory -> enterCategory()
             is NavScreen.EnterPayment -> enterPayment(navScreen)
@@ -32,23 +26,16 @@ interface NavigationController : ActivityController {
         }
     }
 
-    private fun openMain(navBack: Boolean = false) {
-        if (navBack) {
-            while (rootFragmentStack.isNotEmpty() && rootFragmentStack.peek().screenTag != MainFragment::class.java.name) {
-                rootFragmentStack.pop()
-            }
-            navRootUp(rootFragmentStack.peek(), addToBackStack = false)
-        } else {
-            navRootUp(MainFragment())
-        }
+    private fun openMain() {
+        navTo(MainFragment(), addToBackStack = false, navUpTo = true)
     }
 
     private fun openCategories() {
-        navRootUp(CategoryFragment(), addToBackStack = false)
+        navTo(CategoryFragment(), addToBackStack = false, navUpTo = true)
     }
 
     private fun enterPayment(enterPayment: NavScreen.EnterPayment) {
-        navRootUp(EnterPaymentFragment.createWithRevealAnimation(enterPayment.revealAnimationSetting))
+        navTo(EnterPaymentFragment.createWithRevealAnimation(enterPayment.revealAnimationSetting))
     }
 
 
@@ -60,7 +47,7 @@ interface NavigationController : ActivityController {
     private fun chooseCategory(chooseCategory: NavScreen.ChooseCategory) {
         val chooseFrag = ChooseCategoryFragment()
         chooseFrag.arguments = Bundle().also { it.putParcelable(ARG_PAYMENT, chooseCategory.payment) }
-        navRootUp(chooseFrag)
+        navTo(chooseFrag)
     }
 
 //    private fun openFeedback() {
@@ -72,70 +59,5 @@ interface NavigationController : ActivityController {
 
     private fun openSettings() {
         TODO("Open Settings")
-    }
-
-    // Helpers for navigation
-    // ----------------------
-    fun setupNav(savedInstanceState: Bundle?) {
-        if (rootFragmentStack.empty()) {
-            rootFragmentStack.push(loadFragment(MainFragment::class.java, savedInstanceState))
-        }
-        rootFragmentStack.peek() attachIfNeededTo this
-    }
-
-    fun saveNavState(outState: Bundle) {
-        currentActivity.supportFragmentManager.putFragment(outState, RootFragment::class.java.name, rootFragmentStack.peek() as Fragment)
-    }
-
-    private fun navRootUp(fragment: RootFragment, addToBackStack: Boolean = true) {
-        val currentRoot = rootFragmentStack.peek()
-        if (addToBackStack) {
-            rootFragmentStack.push(fragment)
-        } else {
-            if (!rootFragmentStack.empty()) {
-                rootFragmentStack.pop()
-            }
-            rootFragmentStack.push(fragment)
-        }
-//        (currentRoot as Fragment).
-        currentActivity.supportFragmentManager.beginTransaction().
-//                setCustomAnimations(rootFragmentStack.peek().transitionIn, currentRoot.transitionOut).
-                replace(fragmentFrame, rootFragmentStack.peek() as Fragment).commitNow()
-    }
-
-    fun handleOnBack(pressBack: () -> Unit) {
-        val currentRoot = rootFragmentStack.pop()
-        if (!currentRoot.navigateBack()) {
-            if (rootFragmentStack.empty()) {
-                pressBack()
-            } else {
-                currentActivity.supportFragmentManager.beginTransaction().
-//                        setCustomAnimations(rootFragmentStack.peek().transitionIn, currentRoot.transitionOut).
-                        replace(fragmentFrame, rootFragmentStack.peek() as Fragment).commitNow()
-            }
-        } else {
-            // If an action was taken within the fragment, keep it on the stack
-            rootFragmentStack.push(currentRoot)
-        }
-    }
-
-    /*
-    Get a T-Type Fragment from the fragment manager via it's tag
-     */
-    private fun <T : RootFragment> loadFragment(fragClass: Class<T>,
-                                                instanceState: Bundle?): RootFragment {
-        val currentFrag = currentActivity.supportFragmentManager.getFragment(instanceState
-                ?: Bundle(), fragClass.name)
-        return if (instanceState != null && currentFrag != null) {
-            currentFrag as RootFragment
-        } else {
-            fragClass.newInstance()
-        }
-    }
-
-    private infix fun RootFragment.attachIfNeededTo(navController: NavigationController) {
-        if (navController.currentActivity.supportFragmentManager.findFragmentByTag(screenTag) == null) {
-            currentActivity.supportFragmentManager.beginTransaction().replace(fragmentFrame, this as Fragment).commitNow()
-        }
     }
 }
