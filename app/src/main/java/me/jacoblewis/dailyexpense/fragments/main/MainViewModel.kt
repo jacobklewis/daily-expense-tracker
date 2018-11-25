0eb859af1330c03b84e9c4226df01ecfff36db8a
Jacob <zerogamerfx@gmail.com>
2018-11-25 12:38:47 -0600

    added category balancer

--- app/src/main/java/me/jacoblewis/dailyexpense/fragments/main/MainViewModel.kt
+++ app/src/main/java/me/jacoblewis/dailyexpense/fragments/main/MainViewModel.kt
@@ -2,11 +2,9 @@ package me.jacoblewis.dailyexpense.fragments.main
 
 import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.ViewModel
-import android.util.Log
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import me.jacoblewis.dailyexpense.data.BalancesDB
-import me.jacoblewis.dailyexpense.data.models.Category
 import me.jacoblewis.dailyexpense.data.models.Payment
 import me.jacoblewis.dailyexpense.data.models.PaymentCategory
 import javax.inject.Inject
@@ -19,17 +17,6 @@ constructor(val db: BalancesDB) : ViewModel() {
 
     init {
         payments.addSource(db.paymentsDao().getAllPayments(), payments::setValue)
-
-        db.categoriesDao().getAllCategories().observeForever {
-            if (it != null && it.isEmpty()) {
-
-                GlobalScope.launch {
-                    Log.e("category id", "${db.categoriesDao().insertCategory(Category("Lunch", "1B5E20", 230.00f))}")
-                }
-            } else {
-                Log.e("Cat", "Already have one!!! :D")
-            }
-        }
     }
 
     // TODO: remove and add to add payment screen
