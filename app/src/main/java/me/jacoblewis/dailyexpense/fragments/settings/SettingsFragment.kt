a4c7ae1ac05f118b236a1ffda0ad891330f4ea00
Jacob <zerogamerfx@gmail.com>
2018-12-08 13:59:36 -0600

    created fragment and viewmodel

---
+++ app/src/main/java/me/jacoblewis/dailyexpense/fragments/settings/SettingsFragment.kt
@@ -0,0 +1,54 @@
+package me.jacoblewis.dailyexpense.fragments.categories
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
+import butterknife.BindView
+import butterknife.OnClick
+import me.jacoblewis.dailyexpense.R
+import me.jacoblewis.dailyexpense.adapters.CategoryController
+import me.jacoblewis.dailyexpense.adapters.ItemDelegate
+import me.jacoblewis.dailyexpense.commons.RootFragmentOptions
+import me.jacoblewis.dailyexpense.data.models.Category
+import me.jacoblewis.dailyexpense.dependency.utils.MyApp
+import me.jacoblewis.dailyexpense.mainActivity.interfaces.nav.NavScreen
+import me.jacoblewis.dailyexpense.mainActivity.interfaces.nav.RootFragment
+
+class SettingsFragment : RootFragment(R.layout.fragment_category_content) { // TODO: add layout!
+    override val options: RootFragmentOptions = RootFragmentOptions(SettingsFragment::class.java, drawerNavId = R.id.menu_item_settings)
+
+    init {
+        MyApp.graph.inject(this)
+    }
+
+    @BindView(R.id.toolbar)
+    lateinit var toolbar: Toolbar
+
+    private val viewModel: SettingsViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(SettingsViewModel::class.java)
+    }
+
+    override fun onViewBound(view: View) {
+        toolbar.title = "Settings"
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationController.linkToolBarToDrawer(toolbar)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    /**
+     * Navigate Back
+     * @return true iff the screen performed navigation
+     */
+    override fun navigateBack(): Boolean {
+        return false
+    }
+}
