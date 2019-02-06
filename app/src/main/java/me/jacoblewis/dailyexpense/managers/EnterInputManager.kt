23eb01b817c3920a407e4a0dd1a14dbe4cc12293
Jacob <zerogamerfx@gmail.com>
2019-02-05 22:30:04 -0600

    Added input pad

---
+++ app/src/main/java/me/jacoblewis/dailyexpense/managers/EnterInputManager.kt
@@ -0,0 +1,24 @@
+package me.jacoblewis.dailyexpense.managers
+
+import androidx.lifecycle.MutableLiveData
+
+class EnterInputManager(private var value: Float = 0f) {
+    val currentValue: MutableLiveData<Float> = MutableLiveData()
+
+    init {
+        applyValue(value)
+    }
+
+    private fun applyValue(v: Float) {
+        value = v
+        currentValue.postValue(v)
+    }
+
+    fun addDigit(digit: Int) {
+        applyValue(value * 10 + digit / 100f)
+    }
+
+    fun removeDigit() {
+        applyValue(((value * 100).toInt() / 10) / 100f)
+    }
+}
