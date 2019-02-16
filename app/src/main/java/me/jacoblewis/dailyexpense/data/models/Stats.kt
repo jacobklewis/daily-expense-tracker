37f562d8c8742189601645c807a4cca06c7587a4
Jacob <zerogamerfx@gmail.com>
2019-02-16 14:50:11 -0600

    Added Category Pie Chart

--- app/src/main/java/me/jacoblewis/dailyexpense/data/models/Stats.kt
+++ app/src/main/java/me/jacoblewis/dailyexpense/data/models/Stats.kt
@@ -1,8 +1,11 @@
 package me.jacoblewis.dailyexpense.data.models
 
+import me.jacoblewis.dailyexpense.commons.StatsType
 import me.jacoblewis.jklcore.components.recyclerview.IdItem
 import java.util.*
 
-data class Stats(val budget: Float) : IdItem<String> {
+data class Stats(val budget: Float,
+                 val categories: List<Category> = listOf(),
+                 val displayType: StatsType = StatsType.Overview) : IdItem<String> {
     override fun getIdentifier(): String = UUID.randomUUID().toString()
 }
